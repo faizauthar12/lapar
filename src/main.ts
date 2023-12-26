003b3f515685f96fb5b227a836f1b93a74abae83
Faiz Authar <faizauthar@gmail.com>
2023-12-27 01:03:21 +0700

    lapar: Tidy up logger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { logger } from './logger.middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(logger);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'verbose', 'debug'],
+  });
   app.enableCors();
   await app.listen(3000);
 }
